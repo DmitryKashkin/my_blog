61b1bf809343746c8772303bad94cef3b252ef0e
DmitryKashkin <7842066@gmail.com>
2022-10-20 21:53:34 +0300

    v4.5

--- my_blog/blog/admin.py
+++ my_blog/blog/admin.py
@@ -3,4 +3,28 @@ from django.contrib import admin
 from .models import *
 
 
-class
+# class CategoryAdmin(admin.ModelAdmin):
+#     list_display = ('id', 'title')
+#     list_display_links = ('id', 'title')
+#     search_fields = ('title',)
+#     prepopulated_fields = {'slug': ('title',)}
+#
+#
+# class TagAdmin(admin.ModelAdmin):
+#     list_display = ('id', 'title')
+#     list_display_links = ('id', 'title')
+#     search_fields = ('title',)
+#     prepopulated_fields = {'slug': ('title',)}
+#
+#
+# class PostAdmin(admin.ModelAdmin):
+#     list_display = ('id', 'title')
+#     list_display_links = ('id', 'title')
+#     search_fields = ('title',)
+#     prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(Category)
+admin.site.register(Tag)
+admin.site.register(Post)
+
